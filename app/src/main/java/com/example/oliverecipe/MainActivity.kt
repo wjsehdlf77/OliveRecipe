b9636072a3fe55591440efe6e0e96333321e46a9
NongDamGom1216 <hyun5735@naver.com>
2022-04-18 02:16:32 +0900

    메뉴 UI 변경

--- app/src/main/java/com/example/oliverecipe/MainActivity.kt
+++ app/src/main/java/com/example/oliverecipe/MainActivity.kt
@@ -2,10 +2,9 @@ package com.example.oliverecipe
 
 import android.Manifest
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
+import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -13,9 +12,9 @@ import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.oliverecipe.databinding.ActivityMainBinding
+import com.example.oliverecipe.navigation.HomeViewFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -33,7 +32,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
 
         //set default screen
-        bottom_navigation.selectedItemId = R.id.action_refrigerator
+        bottom_navigation.selectedItemId = R.id.action_home
+
+
 
 
     }
@@ -47,25 +48,27 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item?.itemId) {
             R.id.action_home -> {
-                var refrigeratorViewFragment = RefrigeratorViewFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, refrigeratorViewFragment).commit()
-                return true
+                bottom_navigation.selectedItemId = R.id.action_home
             }
 
-            R.id.action_call -> {
-                val intent = Intent(Intent.ACTION_DIAL)
-                intent.data = Uri.parse("tel:031-123-4567")
-                if (intent.resolveActivity(packageManager) != null) {
-                    startActivity(intent)
-                }
+            R.id.menu_add_camera -> {
+                bottom_navigation.selectedItemId = R.id.action_refrigerator
+                // 추가
+
+            }
+
+            R.id.menu_add_gallary -> {
+                bottom_navigation.selectedItemId = R.id.action_refrigerator
+                // 추가
+
+            }
+
+            R.id.action_info -> {
+                Toast.makeText(this,"버전 정보", Toast.LENGTH_SHORT).show()
                 return true
             }
-            R.id.action_send_text -> {
-                Toast.makeText(this,"문자 보내기", Toast.LENGTH_SHORT).show()
-                return true
-            }
-            R.id.action_email -> {
-                Toast.makeText(this,"이메일 보내기", Toast.LENGTH_SHORT).show()
+            R.id.action_setting -> {
+                Toast.makeText(this,"설정", Toast.LENGTH_SHORT).show()
                 return true
             }
         }
@@ -97,9 +100,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 supportFragmentManager.beginTransaction().replace(R.id.main_content, bagFragment).commit()
                 return true
             }
-            R.id.action_search ->{
-                var searchFragment = SearchViewFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, searchFragment).commit()
+            R.id.action_home -> {
+                var homeViewFragment = HomeViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, homeViewFragment).commit()
                 return true
             }
         }
--- app/src/main/java/com/example/oliverecipe/navigation/HomeViewFragment.kt
+++ app/src/main/java/com/example/oliverecipe/navigation/HomeViewFragment.kt
@@ -1,20 +1,19 @@
-package com.example.oliverecipe
+package com.example.oliverecipe.navigation
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.oliverecipe.R
 
-class SearchViewFragment : Fragment() {
-
+class HomeViewFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_search, container, false)
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_home, container, false)
         return view
     }
-
 }
