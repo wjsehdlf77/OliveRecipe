86099f59fed3257582fd634848c6a584ca29a29d
NongDamGom1216 <hyun5735@naver.com>
2022-04-18 00:06:51 +0900

    홈 버튼 추가

--- app/src/main/java/com/example/oliverecipe/MainActivity.kt
+++ app/src/main/java/com/example/oliverecipe/MainActivity.kt
@@ -1,18 +1,31 @@
 package com.example.oliverecipe
 
 import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.oliverecipe.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -24,6 +37,42 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.action_home -> {
+                var refrigeratorViewFragment = RefrigeratorViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, refrigeratorViewFragment).commit()
+                return true
+            }
+
+            R.id.action_call -> {
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:031-123-4567")
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+                return true
+            }
+            R.id.action_send_text -> {
+                Toast.makeText(this,"문자 보내기", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.action_email -> {
+                Toast.makeText(this,"이메일 보내기", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
         toolbar_title_image.visibility = View.VISIBLE
