582e564cb1d2d65b50de11f69af583e67fc0d2d8
soo <sujinsnla@gmail.com>
2022-04-26 00:49:56 +0900

    Mainpage, done

--- app/src/main/java/com/example/oliverecipe/navigation/HomeViewFragment.kt
+++ app/src/main/java/com/example/oliverecipe/navigation/HomeViewFragment.kt
@@ -1,19 +1,42 @@
 package com.example.oliverecipe.navigation
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.oliverecipe.MainActivity
 import com.example.oliverecipe.R
+import com.example.oliverecipe.databinding.FragmentFoodbankBinding
+import com.example.oliverecipe.databinding.FragmentHomeBinding
+import com.example.oliverecipe.navigation.view.oliveListAdapter
 
 class HomeViewFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_home, container, false)
-        return view
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.areyclerView.layoutManager = LinearLayoutManager(context)
+
+        oliveData.getOliveData("토마토") {
+            binding.areyclerView.adapter = oliveListAdapter(it.cOOKRCP01?.row!!)
+        }
     }
+
 }
